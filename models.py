5a5c70ad2f283af87fdf13fef0f26545ff864361
stealth-black-21 <pranavvkumar21@gmail.com>
2020-09-10 18:01:30 +0530

    added model and train funnction

---
+++ models.py
@@ -0,0 +1,13 @@
+import numpy as np
+from keras.layers import Input, Dense
+from keras.models import Model
+
+def model1(input_shape, layers):
+    X_in = Input((input_shape,))
+    X = X_in
+    for i in range(input_shape,0,-(input_shape//layers)):
+        X = Dense(i)(X)
+        Y = Dense(1)(X)
+    model = Model(inputs = X_in, outputs = Y, name='basic1')
+    #model.summary()
+    return model
--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,9 @@
 import csv
 import numpy as np
 from sklearn.utils import shuffle
-
+from models import *
+import keras
+import tensorflow as tf
 
 def getdata(filename):
     File = open(filename)
@@ -13,7 +15,7 @@ def getxandy(*args):
     X  = np.concatenate((args), axis=0)
     Y = []
     for idx,arg in enumerate(args):
-        y = np.ones((arg.shape[0],1))*idx
+        y = np.ones((arg.shape[0],))*idx
         Y.append(y)
     Y = np.concatenate((Y), axis = 0)
     return X,Y
@@ -22,12 +24,26 @@ def savefile(**kwargs):
     for key,File in kwargs.items():
         np.save(key+".npy",File)
 
+def train_data(model, X, Y):
+    opt = keras.optimizers.Adam(learning_rate=0.000003)
+    model.compile(optimizer=opt,
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=['accuracy'])
+    model.fit(X,Y, epochs=epoch, batch_size=batchsize)
 
 
 if __name__ == "__main__":
+
+    input_shape =188
+    layers = 20
+    batchsize = 32
+    epoch = 100
 
     normal = getdata("ptbdb_normal.csv")
     abnormal = getdata("ptbdb_abnormal.csv")
     X,Y = getxandy(normal,abnormal)
     X,Y = shuffle(X,Y)
-    savefile(Xtrain = X, Ytrain = Y)
+    print(type(X[0,1]))
+    #savefile(Xtrain = X, Ytrain = Y)
+    model = model1(188,20)
+    #train_data(model, X, Y)
